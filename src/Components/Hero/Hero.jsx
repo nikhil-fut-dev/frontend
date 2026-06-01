429c8e5e14022508671f7dfe2dfcf60d2d3a015b
NIKHIL <nikhil.fut.dev@gmail.com>
2026-06-01 16:27:28 +0530

    modified:   src/Components/Hero/Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -17,9 +17,13 @@ const Hero = () => {
           <p>collections</p>
           <p>for everyone</p>
         </div>
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="" />
+        </div>
       </div>
       <div className="hero-right">
-
+        <img src={hero_image} alt="" />
       </div>
     </div>
   )
