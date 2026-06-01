ee867b1914cb99e92d0e0f1f53b4f823c17f7855
NIKHIL <nikhil.fut.dev@gmail.com>
2026-06-01 22:54:51 +0530

    modified:   src/Components/Item/Item.jsx modified:   src/Components/Offers/Offers.css modified:   src/Components/Popular/Popular.jsx

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -8,7 +8,7 @@ const Popular = () => {
     <div className='popular'>
       <h1>POPULAR IN WOMEN</h1>
       <hr />
-      <div className="popula-item">
+      <div className="popular-item">
         {data_product.map((item,i)=>{
             return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
         })}
