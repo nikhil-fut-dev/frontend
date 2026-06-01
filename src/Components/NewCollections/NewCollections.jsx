6fe965a60d73eeb96a11561305688eb83d908023
ANCHAL MAURYA <anchal.tech.pro@gmail.com>
2026-06-01 23:35:08 +0530

    modified:   src/Components/NewCollections/NewCollections.css modified:   src/Components/NewCollections/NewCollections.jsx modified:   src/Pages/Shop.jsx

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './NewCollections.css'
-import new_collection from '../Assets/new_collections'
+import new_collections from '../Assets/new_collections'
+import Item from '../Item/Item'
 
 const NewCollections = () => {
   return (
@@ -8,7 +9,9 @@ const NewCollections = () => {
       <h1>NEW COLLECTIONS</h1>
       <hr />
       <div className="collections">
-        {new_collection.map((item,))}
+        {new_collections.map((item,i)=>{
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        })}
       </div>
     </div>
   )
